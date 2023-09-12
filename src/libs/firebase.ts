8bd67d82416637c6f24b882e0989593ba8dc305b
a.kennedy <allankennedy.fernandes@gmail.com>
2023-09-12 11:55:03 -0300

    config firebase

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,13 +1,22 @@
 import { initializeApp} from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBv0QMc1KlH_e8_9l_LqxEcmuT48_-8aAY",
+//   authDomain: "condohelp-ee1c2.firebaseapp.com",
+//   projectId: "condohelp-ee1c2",
+//   storageBucket: "condohelp-ee1c2.appspot.com",
+//   messagingSenderId: "761749792174",
+//   appId: "1:761749792174:web:d0456d81fbb66b914b943f"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBv0QMc1KlH_e8_9l_LqxEcmuT48_-8aAY",
-  authDomain: "condohelp-ee1c2.firebaseapp.com",
-  projectId: "condohelp-ee1c2",
-  storageBucket: "condohelp-ee1c2.appspot.com",
-  messagingSenderId: "761749792174",
-  appId: "1:761749792174:web:d0456d81fbb66b914b943f"
+  apiKey: "AIzaSyA2mGxcjq9SDdqlPGrE9ejAKizfYdzPODk",
+  authDomain: "condohelpdf-6704e.firebaseapp.com",
+  projectId: "condohelpdf-6704e",
+  storageBucket: "condohelpdf-6704e.appspot.com",
+  messagingSenderId: "839865415815",
+  appId: "1:839865415815:web:12e78c626a6e88db486022"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
